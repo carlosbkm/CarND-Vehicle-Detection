ca5d12d22a72ac214ff4729e9ee3d86c13a09298
carlosbkm <carlos.garcia.mejias@gmail.com>
2017-05-21 20:22:50 +0200

    Added folder with material for studying the lessons

---
+++ lessons/lessons_vehicle_tracking.py
@@ -0,0 +1,402 @@
+
+# coding: utf-8
+
+# In[24]:
+
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+#get_ipython().magic('matplotlib inline')
+
+# Read in the image
+image = mpimg.imread('cutout1.jpg')
+
+# Take histograms in R, G, and B
+rhist = np.histogram(image[:,:,0], bins=32, range=(0, 256))
+ghist = np.histogram(image[:,:,1], bins=32, range=(0, 256))
+bhist = np.histogram(image[:,:,2], bins=32, range=(0, 256))
+plt.imshow(image)
+
+
+# In[25]:
+
+# Generating bin centers
+bin_edges = rhist[1]
+bin_centers = (bin_edges[1:]  + bin_edges[0:len(bin_edges)-1])/2
+
+
+# In[26]:
+
+# Plot a figure with all three bar charts
+fig = plt.figure(figsize=(12,3))
+plt.subplot(131)
+plt.bar(bin_centers, rhist[0])
+plt.xlim(0, 256)
+plt.title('R Histogram')
+plt.subplot(132)
+plt.bar(bin_centers, ghist[0])
+plt.xlim(0, 256)
+plt.title('G Histogram')
+plt.subplot(133)
+plt.bar(bin_centers, bhist[0])
+plt.xlim(0, 256)
+plt.title('B Histogram')
+
+
+# In[27]:
+
+hist_features = np.concatenate((rhist[0], ghist[0], bhist[0]))
+hist_features
+
+
+# In[28]:
+
+def color_hist(img, nbins=32, bins_range=(0, 256)):
+    # Compute the histogram of the RGB channels separately
+    rhist = np.histogram(img[:,:,0], bins=nbins, range=bins_range)
+    ghist = np.histogram(img[:,:,1], bins=nbins, range=bins_range)
+    bhist = np.histogram(img[:,:,2], bins=nbins, range=bins_range)
+    # Generating bin centers
+    bin_edges = rhist[1]
+    bin_centers = (bin_edges[1:]  + bin_edges[0:len(bin_edges)-1])/2
+    # Concatenate the histograms into a single feature vector
+    hist_features = np.concatenate((rhist[0], ghist[0], bhist[0]))
+    # Return the individual histograms, bin_centers and feature vector
+    return rhist, ghist, bhist, bin_centers, hist_features
+
+
+# In[29]:
+
+rhist, ghist, bhist, bin_centers, hist_features = color_hist(image)
+
+
+# In[30]:
+
+hist_features
+
+
+# In[31]:
+
+fig = plt.figure(figsize=(12,3))
+plt.subplot(131)
+plt.bar(bin_centers, rhist[0])
+plt.xlim(0, 256)
+plt.title('R Histogram')
+plt.subplot(132)
+plt.bar(bin_centers, ghist[0])
+plt.xlim(0, 256)
+plt.title('G Histogram')
+plt.subplot(133)
+plt.bar(bin_centers, bhist[0])
+plt.xlim(0, 256)
+plt.title('B Histogram')
+fig.tight_layout()
+
+
+# In[32]:
+
+from mpl_toolkits.mplot3d import Axes3D
+
+def plot3d(pixels, colors_rgb,
+        axis_labels=list("RGB"), axis_limits=[(0, 255), (0, 255), (0, 255)]):
+    """Plot pixels in 3D."""
+
+    # Create figure and 3D axes
+    fig = plt.figure(figsize=(8, 8))
+    ax = Axes3D(fig)
+
+    # Set axis limits
+    ax.set_xlim(*axis_limits[0])
+    ax.set_ylim(*axis_limits[1])
+    ax.set_zlim(*axis_limits[2])
+
+    # Set axis labels and sizes
+    ax.tick_params(axis='both', which='major', labelsize=14, pad=8)
+    ax.set_xlabel(axis_labels[0], fontsize=16, labelpad=16)
+    ax.set_ylabel(axis_labels[1], fontsize=16, labelpad=16)
+    ax.set_zlabel(axis_labels[2], fontsize=16, labelpad=16)
+
+    # Plot pixel values with colors given in colors_rgb
+    ax.scatter(
+        pixels[:, :, 0].ravel(),
+        pixels[:, :, 1].ravel(),
+        pixels[:, :, 2].ravel(),
+        c=colors_rgb.reshape((-1, 3)), edgecolors='none')
+
+    return ax  # return Axes3D object for further manipulation
+
+
+# In[33]:
+
+# Read a color image
+#img = cv2.imread("000275.png")
+import glob
+images = []
+for filename in glob.glob('./*.png'): #assuming gif
+    im = cv2.imread(filename)
+    images.append(im)
+img = images[4]
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+# Select a small fraction of pixels to plot by subsampling it
+scale = max(img.shape[0], img.shape[1], 64) / 64  # at most 64 rows and columns
+img_small = cv2.resize(img, (np.int(img.shape[1] / scale), np.int(img.shape[0] / scale)), interpolation=cv2.INTER_NEAREST)
+
+# Convert subsampled image to desired color space(s)
+img_small_RGB = cv2.cvtColor(img_small, cv2.COLOR_BGR2RGB)  # OpenCV uses BGR, matplotlib likes RGB
+img_small_HSV = cv2.cvtColor(img_small, cv2.COLOR_BGR2HSV)
+img_small_LUV = cv2.cvtColor(img_small, cv2.COLOR_BGR2LUV)
+img_small_rgb = img_small_RGB / 255.  # scaled to [0, 1], only for plotting
+
+# Plot and show
+plot3d(img_small_RGB, img_small_rgb)
+plt.show()
+
+plot3d(img_small_HSV, img_small_rgb, axis_labels=list("HSV"))
+plt.show()
+
+plot3d(img_small_LUV, img_small_rgb, axis_labels=list("LUV"))
+plt.show()
+
+
+# In[34]:
+
+image = mpimg.imread('test_img.jpg')
+small_img = cv2.resize(image, (32, 32))
+print(image.shape)
+print(small_img.shape)
+plt.imshow(small_img)
+
+
+# In[35]:
+
+feature_vec = small_img.ravel()
+print(feature_vec.shape)
+
+
+# In[36]:
+
+image = mpimg.imread('cutout1.jpg')
+# Pass the color_space flag as 3-letter all caps string
+# like 'HSV' or 'LUV' etc.
+# KEEP IN MIND IF YOU DECIDE TO USE THIS FUNCTION LATER
+# IN YOUR PROJECT THAT IF YOU READ THE IMAGE WITH 
+# cv2.imread() INSTEAD YOU START WITH BGR COLOR!
+def bin_spatial(img, color_space='RGB', size=(32, 32)):
+    # Convert image to new color space (if specified)
+    if color_space != 'RGB':
+        if color_space == 'HSV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        elif color_space == 'LUV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
+        elif color_space == 'HLS':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+        elif color_space == 'YUV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+        elif color_space == 'YCrCb':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+    else: feature_image = np.copy(img)             
+    # Use cv2.resize().ravel() to create the feature vector
+    features = cv2.resize(feature_image, size).ravel() 
+    # Return the feature vector
+    return features
+
+
+# In[37]:
+
+feature_vec = bin_spatial(image, color_space='RGB', size=(32, 32))
+
+# Plot features
+# plt.plot(feature_vec)
+# plt.title('Spatially Binned Features')
+
+
+# In[38]:
+
+#from skimage import color, exposure
+# images are divided up into vehicles and non-vehicles
+
+images = glob.glob('./**/*.jpeg', recursive=True)
+cars = []
+notcars = []
+
+for image in images:
+    if 'image' in image or 'extra' in image:
+        notcars.append(image)
+    else:
+        cars.append(image)
+
+
+# In[39]:
+
+print('Total cars:', len(cars))
+print('Total not cars:', len(notcars))
+
+
+# In[40]:
+
+# Define a function to return some characteristics of the dataset 
+def data_look(car_list, notcar_list):
+    data_dict = {}
+    # Define a key in data_dict "n_cars" and store the number of car images
+    data_dict["n_cars"] = len(car_list)
+    # Define a key "n_notcars" and store the number of notcar images
+    data_dict["n_notcars"] = len(notcar_list)
+    # Read in a test image, either car or notcar
+    example_img = mpimg.imread(car_list[0])
+    # Define a key "image_shape" and store the test image shape 3-tuple
+    data_dict["image_shape"] = example_img.shape
+    # Define a key "data_type" and store the data type of the test image.
+    data_dict["data_type"] = example_img.dtype
+    # Return data_dict
+    return data_dict
+
+
+# In[41]:
+
+data_info = data_look(cars, notcars)
+
+print('Your function returned a count of', 
+      data_info["n_cars"], ' cars and', 
+      data_info["n_notcars"], ' non-cars')
+print('of size: ',data_info["image_shape"], ' and data type:', 
+      data_info["data_type"])
+
+
+# In[42]:
+
+# Just for fun choose random car / not-car indices and plot example images   
+car_ind = np.random.randint(0, len(cars))
+notcar_ind = np.random.randint(0, len(notcars))
+    
+# Read in car / not-car images
+car_image = mpimg.imread(cars[car_ind])
+notcar_image = mpimg.imread(notcars[notcar_ind])
+# Plot the examples
+fig = plt.figure()
+plt.subplot(121)
+plt.imshow(car_image)
+plt.title('Example Car Image')
+plt.subplot(122)
+plt.imshow(notcar_image)
+plt.title('Example Not-car Image')
+
+
+# In[43]:
+
+from skimage.feature import hog
+# Define a function to return HOG features and visualization
+def get_hog_features(img, orient, pix_per_cell, cell_per_block, vis=False, feature_vec=True, t_sqrt=False):
+    if vis == True:
+        features, hog_image = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
+                                  cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=t_sqrt, 
+                                  visualise=True, feature_vector=False)
+        return features, hog_image
+    else:      
+        features = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
+                       cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=t_sqrt, 
+                       visualise=False, feature_vector=feature_vec)
+        return features
+
+
+# In[44]:
+
+# Generate a random index to look at a car image
+ind = np.random.randint(0, len(cars))
+# Read in the image
+image = mpimg.imread(cars[ind])
+gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+# Define HOG parameters
+orient = 9
+pix_per_cell = 8
+cell_per_block = 2
+# Call our function with vis=True to see an image output
+features, hog_image = get_hog_features(gray, orient, 
+                        pix_per_cell, cell_per_block, 
+                        vis=True, feature_vec=False)
+
+
+# Plot the examples
+fig = plt.figure()
+plt.subplot(121)
+plt.imshow(image, cmap='gray')
+plt.title('Example Car Image')
+plt.subplot(122)
+plt.imshow(hog_image, cmap='gray')
+plt.title('HOG Visualization')
+
+
+# In[45]:
+
+# Define a function to extract features from a list of images
+# Have this function call bin_spatial() and color_hist()
+def extract_features(imgs, cspace='RGB', spatial_size=(32, 32),
+                        hist_bins=32, hist_range=(0, 256)):
+    # Create a list to append feature vectors to
+    features = []
+    # Iterate through the list of images
+    for file in imgs:
+        # Read in each one by one
+        image = mpimg.imread(file)
+        # apply color conversion if other than 'RGB'
+        if cspace != 'RGB':
+            if cspace == 'HSV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+            elif cspace == 'LUV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2LUV)
+            elif cspace == 'HLS':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+            elif cspace == 'YUV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+        else: feature_image = np.copy(image)      
+        # Apply bin_spatial() to get spatial color features
+        spatial_features = bin_spatial(feature_image, size=spatial_size)
+        # Apply color_hist() also with a color space option now
+        hist_features = color_hist(feature_image, nbins=hist_bins, bins_range=hist_range)
+        # Append the new feature vector to the features list
+        features.append(np.concatenate((spatial_features, hist_features)))
+    # Return list of feature vectors
+    return features
+
+
+# In[47]:
+
+car_features = extract_features(cars, cspace='RGB', spatial_size=(32, 32),
+                        hist_bins=32, hist_range=(0, 256))
+notcar_features = extract_features(notcars, cspace='RGB', spatial_size=(32, 32),
+                        hist_bins=32, hist_range=(0, 256))
+
+
+# In[46]:
+from sklearn.preprocessing import StandardScaler
+
+
+if len(car_features) > 0:
+    # Create an array stack of feature vectors
+   # from IPython.core.debugger import Tracer; Tracer()() 
+    X = np.vstack((car_features, notcar_features)).astype(np.float64)                        
+    # Fit a per-column scaler
+    X_scaler = StandardScaler().fit(X)
+    # Apply the scaler to X
+    scaled_X = X_scaler.transform(X)
+    car_ind = np.random.randint(0, len(cars))
+    # Plot an example of raw and scaled features
+    fig = plt.figure(figsize=(12,4))
+    plt.subplot(131)
+    plt.imshow(mpimg.imread(cars[car_ind]))
+    plt.title('Original Image')
+    plt.subplot(132)
+    plt.plot(X[car_ind])
+    plt.title('Raw Features')
+    plt.subplot(133)
+    plt.plot(scaled_X[car_ind])
+    plt.title('Normalized Features')
+    fig.tight_layout()
+else: 
+    print('Your function only returns empty feature vectors...')
+
+
+# In[ ]:
+
+
+
